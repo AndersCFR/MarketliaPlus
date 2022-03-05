628b339957c338806f0cbeb3bc3d9b9fa2c64994
Javier Erazo <alex.erazo01@epn.edu.ec>
2022-03-05 16:08:01 -0500

    update/inicio/ Pagina inicio done!

--- marketliaWeb/src/app/pages/inicio/inicio-page/inicio-page.component.ts
+++ marketliaWeb/src/app/pages/inicio/inicio-page/inicio-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoAnuncio } from 'src/app/interfaces/infoanununcio.interface';
 
 @Component({
   selector: 'app-inicio-page',
@@ -7,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPageComponent implements OnInit {
 
+  infoAnuncios: InfoAnuncio[]=[
+    {
+      urlImg:'https://img10.naventcdn.com/home/RPHOMv4.8.0-RC1/images/help-image-buy.jpg',
+      mensajeAnuncio: '¿Qué necesito para comprar?',
+      mensajeButton: 'Guía de Compra'
+    },
+    {
+      urlImg: 'https://img10.naventcdn.com/home/RPHOMv4.8.0-RC1/images/help-image-rent.jpg',
+      mensajeAnuncio: '¿Qué necesito para alquilar?',
+      mensajeButton: 'Guía de Compra'
+    },
+    {
+      urlImg: 'https://img10.naventcdn.com/home/RPHOMv4.8.0-RC1/images/help-image-sell.jpg',
+      mensajeAnuncio: '¿Qué necesito para vender?',
+      mensajeButton: 'Guía de Compra'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- marketliaWeb/src/app/pages/inicio/inicio.module.ts
+++ marketliaWeb/src/app/pages/inicio/inicio.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { InicioPageComponent } from './inicio-page/inicio-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BuscadorComponent } from './buscador/buscador.component';
+import { InfoAnuncioComponent } from './info-anuncio/info-anuncio.component';
 
 
 
@@ -10,7 +11,8 @@ import { BuscadorComponent } from './buscador/buscador.component';
 @NgModule({
   declarations: [
     InicioPageComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    InfoAnuncioComponent
   ],
   imports: [
     CommonModule,
