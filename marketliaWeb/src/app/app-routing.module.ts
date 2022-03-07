8dd308802339e827203e35b1b5b7d898cfc7f3b8
Javier Erazo <alex.erazo01@epn.edu.ec>
2022-03-07 00:01:01 -0500

    create/nuevapub/ formulario nueva publicacion

--- marketliaWeb/src/app/app-routing.module.ts
+++ marketliaWeb/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioPageComponent } from './pages/inicio/inicio-page/inicio-page.component';
+import { NuevaPublicacionPageComponent } from './pages/nueva-publicacion/nueva-publicacion-page/nueva-publicacion-page.component';
 
 
 
@@ -9,10 +10,10 @@ const routes: Routes = [
     path:'inicio',
     component: InicioPageComponent
   },
-  // {
-  //   path: 'publicar',
-  //   component:
-  // }
+  {
+    path: 'publicar',
+    component: NuevaPublicacionPageComponent
+  },
   {
     path: '**',
     pathMatch: 'full',
--- marketliaWeb/src/app/pages/nueva-publicacion/nueva-publicacion.module.ts
+++ marketliaWeb/src/app/pages/nueva-publicacion/nueva-publicacion.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NuevaPublicacionPageComponent } from './nueva-publicacion-page/nueva-publicacion-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormNuevaPubComponent } from './form-nueva-pub/form-nueva-pub.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NuevaPublicacionPageComponent,
+    FormNuevaPubComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ]
 })
 export class NuevaPublicacionModule { }
